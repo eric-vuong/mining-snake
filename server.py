f403d3ebe4c1c9d35e32b37cc1372f333346e805
eric-vuong <61363588+eric-vuong@users.noreply.github.com>
2021-08-11 18:54:26 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-import os
+#!/usr/bin/python
 
+import os
+import argparse
 import cherrypy
+import time
 
 import server_logic
 
@@ -9,6 +12,9 @@ class BattlesnakeServer(object):
     This is a simple Battlesnake server written in Python using the CherryPy Web Framework.
     For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
     """
+    ready=True
+    name=''
+    debug=0
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -29,6 +35,8 @@ class BattlesnakeServer(object):
             "color": "#000000",  # TODO: Personalize
             "head": "default",  # TODO: Personalize
             "tail": "default",  # TODO: Personalize
+            "ready": self.ready,
+            "name": self.name
         }
 
     @cherrypy.expose
@@ -38,9 +46,10 @@ class BattlesnakeServer(object):
         This function is called everytime your snake is entered into a game.
         cherrypy.request.json contains information about the game that's about to be played.
         """
+        self.ready=False
         data = cherrypy.request.json
         
-        print(f"{data['game']['id']} START")
+        if self.debug == 1 : print(f"{data['game']['id']} START")
         return "ok"
 
     @cherrypy.expose
@@ -70,25 +79,59 @@ class BattlesnakeServer(object):
         """
         data = cherrypy.request.json
         win = -1
+        #print(data)
         #print(f"{data['game']['id']} END")
-        if len(data['board']['snakes']) == 1:
-          for i in data['board']['snakes']:
-            if (i['name'] == "mining-snake"):
-              print("we won")
-              win = 1
+        if data['board']['snakes'] != None: # it wasn't a tie
+            if data['board']['snakes'][0]['id'] == data['you']['id']:
+                print("we won")
+                win = 1
             else:
-              print("we lost")
+                print("we lost")
         else:
-          print("we lost")
-        server_logic.postgame(win)
+            print("it was a tie:we lost")
+        ret,loss,rewardsum = server_logic.postgame(win)
+        # if the model updated, save the game lenght
+        if ret != 0 : 
+            f = open('./pref/'+name,'a')
+            f.write(str(ret) + ","  + str(data['turn']) + ","+str(loss)+ "," + str(rewardsum) +"\n")
+            f.close()
+        self.ready = True
         return "ok"
 
+    @cherrypy.expose
+    def save(self):
+        """
+        This function will tell the snake to save a copy of itself.
+        """
+        path="./backup/" + self.name + time.strftime("%Y%m%d%H%M%S")
+        server_logic.save(path)
+        
+        return "ok"
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def load(self):
+        data = cherrypy.request.json
+        if data['path'] != None : server_logic.open(data['path'])
+        else : return 400
+        return 200
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--Port', default="8080", help='The port that server will be available on.')
+    parser.add_argument('-n', '--Name', default="Snake", help='The name of the snake, used for logging.')
+    parser.add_argument('-l', '--Load', default=None, help='Filepath to a model to load and run for this snake')
+    args = vars(parser.parse_args())
+    port = args["Port"]
+    name = args["Name"]
+    print(port)
+
     server = BattlesnakeServer()
+    server.name = name
+
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+        {"server.socket_port": int(os.environ.get("PORT", port)),}
     )
     print("Starting Battlesnake Server...")
     cherrypy.quickstart(server)
--- server_logic.py
+++ server_logic.py
@@ -5,26 +5,27 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+debug=0
+
 optimizer = keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
 # Configuration paramaters for the whole setup
 seed = 1337
 gamma = 0.99  # Discount factor for past rewards
 epsilon = 1.0  # Epsilon greedy parameter
-epsilon_min = 0.1  # Minimum epsilon greedy parameter
+# Lowered min to 0.01 from 0.1 because it results in self collisions
+epsilon_min = 0.01  # Minimum epsilon greedy parameter
 epsilon_max = 1.0  # Maximum epsilon greedy parameter
-epsilon_interval = (
-    epsilon_max - epsilon_min
-)  # Rate at which to reduce chance of random action being taken
+epsilon_interval = (epsilon_max - epsilon_min)  # Rate at which to reduce chance of random action being taken
 batch_size = 32  # Size of batch taken from replay buffer
 
+# Create neural network
 def create_q_model():
-# create and return keras nn
     # Network defined by the Deepmind paper
-    # 11 11 3 is rgb
+    # Input is 11x11 with 3 channels (rgb)
     inputs = layers.Input(shape=(11, 11, 3,))
 
     # Convolutions on the frames on the screen
-    # issue is the kernel sizes 8 4 3, fix is setting the next 2 layers to same padding
+    # Same padding on layers 2 and 3 are required to function
     layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
     layer2 = layers.Conv2D(64, 4, strides=2, activation="relu", padding='same')(layer1)
     layer3 = layers.Conv2D(64, 3, strides=1, activation="relu", padding='same')(layer2)
@@ -35,9 +36,9 @@ def create_q_model():
     action = layers.Dense(4, activation="linear")(layer5)
 
     return keras.Model(inputs=inputs, outputs=action)
+# Create models
 model = create_q_model()
 model_target = create_q_model()
-# create model and model_target
 
 # Experience replay buffers
 action_history = []
@@ -45,101 +46,115 @@ state_history = []
 state_next_history = []
 rewards_history = []
 done_history = []
-#episode_reward_history = []
-#running_reward = 0
-#episode_count = 0
+running_reward = 0 # sum of scores
+episode_count = 0 # number of games played
 frame_count = 0
 # Number of frames to take random action and observe output
-epsilon_random_frames = 50000
+epsilon_random_frames = 500
 # Number of frames for exploration
-epsilon_greedy_frames = 1000000.0
+# lowered it from 1 000 000 to 100 000
+epsilon_greedy_frames = 100000
 # Maximum replay length
 # Note: The Deepmind paper suggests 1000000 however this causes memory issues
 max_memory_length = 100000
-# Train the model after 4 actions
-update_after_actions = 4
+
 # How often to update the target network
-# lowed it from 10000 to 10, such that approx every tenth game it updates
-update_target_network = 10
+# lowered it from 10000 to 100, so it updates every 100 games
+update_target_network = 100
 # Using huber loss for stability
 loss_function = keras.losses.Huber()
 
+# Counter to track number of snakes
+snake_count = -1
+
+# Choose a move given the current game information
 def choose_move(data: dict) -> str:
-    """
-    data: Dictionary of all Game Board data as received from the Battlesnake Engine.
-    For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
-
-    return: A String, the single move to make. One of "up", "down", "left" or "right".
-
-    Use the information in 'data' to decide your next move. The 'data' variable can be interacted
-    with as a Python Dictionary, and contains all of the information about the Battlesnake board 
-    for each move of the game.
-
-    """
+        
     # Random action for exploration
-    possible_moves = [0,1,2,3] #["up", "down", "left", "right"]
+    possible_moves = [0,1,2,3] #These correspond to ["up", "down", "left", "right"]
     global frame_count
     global epsilon_random_frames
     global epsilon
+    global snake_count
+    global episode_count
     frame_count += 1
+    # Turn 0 setup
+    if data['turn'] == 0:
+        # Set number of snakes for previous turn
+        snake_count = -1
+        # add 1 episode for each first turn seen
+        episode_count += 1
 
-    # process board data
+    # Process board data into 11x11 image with 3 channels
+    # Health is represented as the brightness of the snake
     x = np.zeros([11,11,3])
 
-    # Put data into rgb format
-# food
+    # Add food
     for food in data['board']['food']:
-      x[food['y']][food['x']][0] = 100
-# self body
+        x[food['y']][food['x']][0] = 100
+    
+    # Add own body parts
     i = len(data['you']['body'])
     for part in data['you']['body']:
         if i == len(data['you']['body']):
-            x[part['y']][part['x']][1] = data['you']['health'] + 2
+            # Head
+            x[part['y']][part['x']][1] = data['you']['health']
             i -= 1
         elif i != 1:
+            # Body
             x[part['y']][part['x']][1] = data['you']['health'] + 1
             i -= 1
         else:
-            x[part['y']][part['x']][1] = data['you']['health']
+            # Tail
+            x[part['y']][part['x']][1] = data['you']['health'] + 2
 
-      # other bodies
+    # Add other snakes
+    current_count = 0
     for snake in data['board']['snakes']:
-        # skip own body
+        # Skip own body
         if data['you']['name'] == snake['name']:
             continue
+        current_count += 1
         j = len(snake['body'])
         for part in snake['body']:
             if j == len(snake['body']):
-                x[part['y']][part['x']][2] = snake['health'] + 2
+                # Head
+                x[part['y']][part['x']][2] = snake['health']
                 j -= 1
             elif j != 1:
+                # Body
                 x[part['y']][part['x']][2] = snake['health'] + 1
                 j -= 1
             else:
-                x[part['y']][part['x']][2] = snake['health']
-    image = np.zeros([11,11])
-    for i in range(11):
-      for j in range(11):
-        image[i][j] = np.sum(x[i][j])
-    print(np.flip(image, 0))
+                # Tail
+                x[part['y']][part['x']][2] = snake['health'] + 2
+    
+    # Get image for debugging
+    if debug == 1:
+        image = np.zeros([11,11])
+        for i in range(11):
+            for j in range(11):
+                image[i][j] = np.sum(x[i][j])
+        print(np.flip(image, 0))
 
-    # random move
+    # Choose move
     if frame_count < epsilon_random_frames or epsilon > np.random.rand(1)[0]:
-    # Take random action
-      move = np.random.choice(possible_moves)
+        # Take random action
+        move = np.random.choice(possible_moves)
     else:
-      # Predict action Q-values
-      # From environment state
-      state_tensor = tf.convert_to_tensor(x)
-      state_tensor = tf.expand_dims(state_tensor, 0)
-      action_probs = model(state_tensor, training=False)
-      # Take best action
-      action = tf.argmax(action_probs[0]).numpy()
-      move = possible_moves[action]
+        # Predict action Q-values
+        # From environment state
+        state_tensor = tf.convert_to_tensor(x)
+        state_tensor = tf.expand_dims(state_tensor, 0)
+        action_probs = model(state_tensor, training=False)
+        # Take best action
+        action = tf.argmax(action_probs[0]).numpy()
+        move = possible_moves[action]
 
-    print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
-    # POST MOVE 
+    if debug == 1 : print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
+    
     # Decay probability of taking random action
+    # After ~100 000 frames the random action drops to 0.01
     global epsilon_interval
     global epsilon_greedy_frames
     epsilon -= epsilon_interval / epsilon_greedy_frames
@@ -152,77 +167,112 @@ def choose_move(data: dict) -> str:
     # Save actions and states in replay buffer
     action_history.append(move)
     state_history.append(x)
-    #state_next_history.append(state_next)
-    done_history.append(0) # change it to 1 in post game
-    # rewards
-    if data['you']['health'] == 100:
-      reward = 1
-    else:
-      reward = 0
-    rewards_history.append(reward)
-
+    # Update next state after the first turn
+    if data['turn'] != 0:
+        state_next_history.append(x)
+    done_history.append(0) # Done is set to one after the game is over
+    
+    # Update reward of previous action
+    if data['turn'] != 0:
+        reward = 0
+        # Reward for eating food: 0.1
+        if data['you']['health'] == 100:
+            reward += 0.1
+        # Reward when another snake dies: 0.5
+        if current_count < snake_count:
+            reward += 0.5
+        # Only update if there is a reward
+        if reward > 0:
+            # Increment most recent reward
+            rewards_history[len(rewards_history) - 1] = rewards
+            
+            # Track global results
+            global running_reward
+            running_reward += reward
+    # Add reward for current action 
+    # Result of this action and its corresponding reward are not yet known
+    rewards_history.append(0)
+    
+    # Update snake_count
+    snake_count = current_count
     return move
 
 # run at the end of games to update
 def postgame(win):
-  # update reward based on win or loss
-  if len(rewards_history) > 0:
-    rewards_history[len(rewards_history) - 1] = win
-  if len(done_history) > 0:
-  # update done
-    done_history[len(done_history) - 1] = 1
+    # Add empty next state
+    state_next_history.append(np.zeros([11,11,3]))
+    # Update reward based on win or loss
+    if len(rewards_history) > 0:
+        rewards_history[len(rewards_history) - 1] = win
+        global running_reward
+        running_reward += win
+        
+    # Update done
+    if len(done_history) > 0:
+        done_history[len(done_history) - 1] = 1
+        
+        
+    # Update based on batch_size after each game
+    if len(done_history) > batch_size:
 
+        # Get indices of samples for replay buffers
+        indices = np.random.choice(range(len(done_history)), size=batch_size)
 
-  # Get indices of samples for replay buffers
-  indices = np.random.choice(range(len(done_history)), size=batch_size)
+        # Using list comprehension to sample from replay buffer
+        state_sample = np.array([state_history[i] for i in indices])
+        state_next_sample = np.array([state_next_history[i] for i in indices])
+        rewards_sample = [rewards_history[i] for i in indices]
+        action_sample = [action_history[i] for i in indices]
+        done_sample = tf.convert_to_tensor(
+            [float(done_history[i]) for i in indices]
+        )
 
-  # Using list comprehension to sample from replay buffer
-  state_sample = np.array([state_history[i] for i in indices])
-  #state_next_sample = np.array([state_next_history[i] for i in indices])
-  rewards_sample = [rewards_history[i] for i in indices]
-  action_sample = [action_history[i] for i in indices]
-  done_sample = tf.convert_to_tensor(
-      [float(done_history[i]) for i in indices]
-  )
+        # Build the updated Q-values for the sampled future states
+        # Use the target model for stability
+        future_rewards = model_target.predict(state_next_sample)
+        # Q value = reward + discount factor * expected future reward
+        updated_q_values = rewards_sample + gamma * tf.reduce_max(
+            future_rewards, axis=1
+        )
 
-  # Build the updated Q-values for the sampled future states
-  # Use the target model for stability
-  future_rewards = model_target.predict(state_sample) #state_next_sample
-  # Q value = reward + discount factor * expected future reward
-  updated_q_values = rewards_sample + gamma * tf.reduce_max(
-      future_rewards, axis=1
-  )
+        # If final frame set the last value to -1
+        updated_q_values = updated_q_values * (1 - done_sample) - done_sample
 
-  # If final frame set the last value to -1
-  updated_q_values = updated_q_values * (1 - done_sample) - done_sample
+        # Create a mask so we only calculate loss on the updated Q-values
+        masks = tf.one_hot(action_sample, 4)
 
-  # Create a mask so we only calculate loss on the updated Q-values
-  masks = tf.one_hot(action_sample, 4)
+        with tf.GradientTape() as tape:
+            # Train the model on the states and updated Q-values
+            q_values = model(state_sample)
 
-  with tf.GradientTape() as tape:
-      # Train the model on the states and updated Q-values
-      q_values = model(state_sample)
+            # Apply the masks to the Q-values to get the Q-value for action taken
+            q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+            # Calculate loss between new Q-value and old Q-value
+            loss = loss_function(updated_q_values, q_action)
 
-      # Apply the masks to the Q-values to get the Q-value for action taken
-      q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
-      # Calculate loss between new Q-value and old Q-value
-      loss = loss_function(updated_q_values, q_action)
+        # Backpropagation
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    
+    # Update every 100 games
+    if episode_count % update_target_network == 0:
+        # Update the the target network with new weights
+        model_target.set_weights(model.get_weights())
+        return frame_count, loss.numpy(), running_reward
 
-  # Backpropagation
-  grads = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(grads, model.trainable_variables))
-  
-  if frame_count % update_target_network == 0:
-    # update the the target network with new weights
-    model_target.set_weights(model.get_weights())
-    # Log details
-    #template = "running reward: {:.2f} at episode {}, frame count {}"
-    #print(template.format(running_reward, episode_count, frame_count))
-  # Limit the state and reward history
-  if len(rewards_history) > max_memory_length:
-    del rewards_history[:1]
-    del state_history[:1]
-    #del state_next_history[:1]
-    del action_history[:1]
-    del done_history[:1]
+    # Limit the state and reward history
+    if len(rewards_history) > max_memory_length:
+        del rewards_history[:1]
+        del state_history[:1]
+        del state_next_history[:1]
+        del action_history[:1]
+        del done_history[:1]
+    return 0, loss.numpy(), running_reward
 
+def save(path):
+    print("saving to "+path)
+    model.save(path)
+
+def open(path):
+    global model
+    model = keras.models.load_model(path)
