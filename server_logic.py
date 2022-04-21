50b1e0a0a9d304f18c5363d07c8c6abebf06078d
eric-vuong <61363588+eric-vuong@users.noreply.github.com>
2022-04-21 02:30:31 -0700

    Updated comments and variable names

--- server_logic.py
+++ server_logic.py
@@ -5,17 +5,19 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-debug=0
+# Setup and deep-q network implementation based on https://keras.io/examples/rl/deep_q_network_breakout/
+debug = 0 # Set to 1 to show additional information
 
-# set to 1 to run without updating weights
-no_training = 1
+no_training = 1 # set to 1 to run without updating weights
 
 optimizer = keras.optimizers.Adam(learning_rate=0.00025, clipnorm=1.0)
+
 # Configuration paramaters for the whole setup
 seed = 1337
 gamma = 0.99  # Discount factor for past rewards
 epsilon = 1.0  # Epsilon greedy parameter
-# Lowered min to 0.01 from 0.1 because it results in self collisions
+
+# Lowered epsilon_min to 0.01 from 0.1 because it results in self collisions
 epsilon_min = 0.01  # Minimum epsilon greedy parameter
 epsilon_max = 1.0  # Maximum epsilon greedy parameter
 epsilon_interval = (epsilon_max - epsilon_min)  # Rate at which to reduce chance of random action being taken
@@ -23,6 +25,7 @@ batch_size = 32  # Size of batch taken from replay buffer
 
 # Create neural network
 def create_q_model():
+
     # Network defined by the Deepmind paper
     # Input is 11x11 with 3 channels (rgb)
     inputs = layers.Input(shape=(11, 11, 3,))
@@ -39,6 +42,7 @@ def create_q_model():
     action = layers.Dense(4, activation="linear")(layer5)
 
     return keras.Model(inputs=inputs, outputs=action)
+    
 # Create models
 model = create_q_model()
 model_target = create_q_model()
@@ -52,121 +56,132 @@ done_history = []
 running_reward = 0 # sum of scores
 episode_count = 0 # number of games played
 frame_count = 0
+
 # Number of frames to take random action and observe output
 epsilon_random_frames = 10000
+
 # Number of frames for exploration
-# lowered it from 1 000 000 to 200 000
+# Lowered from 1 000 000
 epsilon_greedy_frames = 20000
+
 # Maximum replay length
 # Note: The Deepmind paper suggests 1000000 however this causes memory issues
 max_memory_length = 100000
 
 # How often to update the target network
-# lowered it from 10000 to 100, so it updates every 100 games
+# Lowered from 10000 so it updates more frequently
 update_target_network = 500
+
 # Using huber loss for stability
 loss_function = keras.losses.Huber()
 
+# End of Atari breakout code
 # Counter to track number of snakes
 snake_count = -1
 
-# Choose a move given the current game information
+# Function converts the current board state into an image
+# and chooses a valid move using the model
 def choose_move(data: dict) -> str:
         
     # Random action for exploration
-    possible_moves = [0,1,2,3] #These correspond to ["up", "down", "left", "right"]
+    possible_moves = [0,1,2,3] # These correspond to "up", "down", "left", "right"
     global frame_count
     global epsilon_random_frames
     global epsilon
     global snake_count
     global episode_count
     frame_count += 1
+    
     # Turn 0 setup
     if data['turn'] == 0:
+    
         # Set number of snakes for previous turn
         snake_count = -1
+        
         # add 1 episode for each first turn seen
         episode_count += 1
 
     # Process board data into 11x11 image with 3 channels
-    # Health is represented as the brightness of the snake
-    x = np.zeros([11,11,3])
+    # First channel is food tiles
+    # Second channel is our snakes
+    # Third channel is enemy snakes
+    # The brightness is based on snake health and body part
+    board_image = np.zeros([11,11,3])
 
     # Add food
     for food in data['board']['food']:
-        x[food['y']][food['x']][0] = 1
+        board_image[food['y']][food['x']][0] = 1
     
-    # Add own body parts
+    # Add our snake's parts
     i = len(data['you']['body'])
     for part in data['you']['body']:
         if i == len(data['you']['body']):
+        
             # Head
-            x[part['y']][part['x']][1] = (data['you']['health'] + 155) / 255
+            board_image[part['y']][part['x']][1] = (data['you']['health'] + 155) / 255
             i -= 1
         elif i != 1:
+        
             # Body
-            x[part['y']][part['x']][1] = (data['you']['health'] + 75) / 255
+            board_image[part['y']][part['x']][1] = (data['you']['health'] + 75) / 255
             i -= 1
         else:
+        
             # Tail
-            x[part['y']][part['x']][1] = data['you']['health'] / 255
+            board_image[part['y']][part['x']][1] = data['you']['health'] / 255
 
-    # Add other snakes
-    current_count = 0
+    # Add other snake parts
+    current_snakes = 0
     for snake in data['board']['snakes']:
-        # Skip own body
+    
+        # Skip our snake
         if data['you']['name'] == snake['name']:
             continue
-        current_count += 1
+        current_snakes += 1
         j = len(snake['body'])
         for part in snake['body']:
             if j == len(snake['body']):
+            
                 # Head
-                x[part['y']][part['x']][2] = (snake['health'] + 155) / 255
+                board_image[part['y']][part['x']][2] = (snake['health'] + 155) / 255
                 j -= 1
             elif j != 1:
+            
                 # Body
-                x[part['y']][part['x']][2] = (snake['health'] + 75) / 255
+                board_image[part['y']][part['x']][2] = (snake['health'] + 75) / 255
                 j -= 1
             else:
+            
                 # Tail
-                x[part['y']][part['x']][2] = snake['health'] / 255
+                board_image[part['y']][part['x']][2] = snake['health'] / 255
     
     # Get image for debugging
     if debug == 1:
         image = np.zeros([11,11])
         for i in range(11):
             for j in range(11):
-                image[i][j] = np.sum(x[i][j])
-        if debug == 1 : print(np.flip(image, 0))
-
-    # Choose move only. Skip updating history
-    if no_training == 1:
-        # Predict action Q-values
-        # From environment state
-        state_tensor = tf.convert_to_tensor(x)
-        state_tensor = tf.expand_dims(state_tensor, 0)
-        action_probs = model(state_tensor, training=False)
-        # Take best action
-        action = tf.argmax(action_probs[0]).numpy()
-        move = possible_moves[action]
-        return move
-
-    # Choose move
+                image[i][j] = np.sum(board_image[i][j])
+        print(np.flip(image, 0))
+        
+    # Section based on train section from https://keras.io/examples/rl/deep_q_network_breakout/
+    # Choose move using epsilon-greedy
     if frame_count < epsilon_random_frames or epsilon > np.random.rand(1)[0]:
+    
         # Take random action
         move = np.random.choice(possible_moves)
     else:
+    
         # Predict action Q-values
         # From environment state
-        state_tensor = tf.convert_to_tensor(x)
+        state_tensor = tf.convert_to_tensor(board_image)
         state_tensor = tf.expand_dims(state_tensor, 0)
         action_probs = model(state_tensor, training=False)
+        
         # Take best action
         action = tf.argmax(action_probs[0]).numpy()
         move = possible_moves[action]
-
-    if debug == 1 : print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
+    if debug == 1:
+        print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
     
     # Decay probability of taking random action
     # After ~100 000 frames the random action drops to 0.01
@@ -174,64 +189,71 @@ def choose_move(data: dict) -> str:
     global epsilon_greedy_frames
     epsilon -= epsilon_interval / epsilon_greedy_frames
     epsilon = max(epsilon, epsilon_min)
-    
-    # Reward needs to be done for the last Turns action
-    # It isnt immediately clear what the reward is until the turn is resolved
-    #episode_reward += reward
 
     # Save actions and states in replay buffer
     action_history.append(move)
-    state_history.append(x)
+    state_history.append(board_image)
+    
+    # End of Atari breakout train code
     # Update next state after the first turn
     if data['turn'] != 0:
-        state_next_history.append(x)
-    done_history.append(0) # Done is set to one after the game is over
+        state_next_history.append(board_image)
+    done_history.append(0) # This is set to one later if it was the last turn
     
-    # Update reward of previous action
+    # Update reward of previous turn
     if data['turn'] != 0:
         reward = 0
-        # Reward for eating food: 0.25
+        
+        # Reward for eating food
         if data['you']['health'] == 100:
             reward += 0.5
-        # Reward when another snake dies: 0.5
-        if current_count < snake_count:
+            
+        # Reward when another snake dies
+        if current_snakes < snake_count:
             reward += 1
+            
         # Only update if there is a reward
         if reward > 0 & len(rewards_history) > 0:
+        
             # Increment most recent reward
             rewards_history[-1] = reward
             
             # Track global results
             global running_reward
             running_reward += reward
-    # Add reward for current action 
-    # Result of this action and its corresponding reward are not yet known
+            
+    # Add reward for current action, which is not yet known
     rewards_history.append(0)
     
     # Update snake_count
-    snake_count = current_count
+    snake_count = current_snakes
     return move
 
-# run at the end of games to update
+# Function runs at the end of a game to update the model
+# Returns the frame count, loss, and running reward
+# Implementation based on train section in https://keras.io/examples/rl/deep_q_network_breakout/
 def postgame(win):
-    # skip
+
+    # Skip updating if training is disabled
     if no_training == 1:
         return 0,0,0
-    # Add empty next state
+        
+    # Add empty next board state
     state_next_history.append(np.zeros([11,11,3]))
+    
     # Update reward based on win or loss
     global running_reward
     if len(rewards_history) > 0:
         rewards_history[-1] += win
         running_reward += win
         
-    # Update done
+    # Update done status
     if len(done_history) > 0:
         done_history[len(done_history) - 1] = 1
         
-    report_loss = None    
-    # Limit the state and reward history
-    # Delete entire history when it exceeds max length
+    report_loss = None  
+    
+    # Limit the state and reward history length
     if len(rewards_history) > max_memory_length:
         del rewards_history[:]
         del state_history[:]
@@ -274,6 +296,7 @@ def postgame(win):
 
             # Apply the masks to the Q-values to get the Q-value for action taken
             q_action = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+            
             # Calculate loss between new Q-value and old Q-value
             loss = loss_function(updated_q_values, q_action)
             report_loss = loss.numpy()
@@ -288,13 +311,15 @@ def postgame(win):
             model_target.set_weights(model.get_weights())
             return frame_count, report_loss, running_reward
         
-    # No loss to return
+    # Return only running reward if no update was done
     return 0, 0, running_reward
 
+# Save model
 def save(path):
     print("saving to "+path)
     model.save(path)
 
+# Open model
 def open(path):
     print("Opening model")
     global model
